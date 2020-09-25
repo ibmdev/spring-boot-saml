a71ad2770c3563a8d40c3d5a08f8ae4988ecce63
ibmdev <ibmdev102017@gmail.com>
2020-09-25 23:22:05 +0200

    Initialisation socle technique

---
+++ src/main/java/fr/sma/rg/ServletInitializer.java
@@ -0,0 +1,13 @@
+package fr.sma.rg;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(RgApplication.class);
+	}
+
+}
---
+++ src/test/java/fr/sma/rg/RgApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.sma.rg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
